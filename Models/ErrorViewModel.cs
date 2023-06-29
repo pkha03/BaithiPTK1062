f04f5654d28e5bd0a96d047b7968a9c7c661ed76
pkha03 <91863540+pkha03@users.noreply.github.com>
2023-06-29 08:48:51 +0700

    BaithiPTK1062

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace BaithiPTK1062.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
